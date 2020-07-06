40f1d403dcaf851377a10417353962a35ad72d33
Glitch (hello-express) <none>
2020-07-06 12:44:57 +0000

    🏉👨‍❤️‍👨 Checkpoint ./komutlar/üyedurum.js:12062196/745 ./komutlar/moderasyon.js:12062196/108 ./komutlar/düello.js:12062196/4311 ./util/Util.js:12062196/3662 ./package.json:12062196/916 ./komutlar/eğlence.js:12062196/83

--- komutlar/üyedurum.js
+++ komutlar/üyedurum.js
@@ -4,10 +4,12 @@ exports.run = (client, message) => {
   let üye = new Discord.MessageEmbed()
     .setAuthor("Üyedurum")
     .setColor("RED")
-    .addField("**Çevrimiçi Kullanıcılar**"{message.guild.members.cache.filter(o => o.presence.status === 'online').size}`)
-  .addField(`**Çevrimiçi Kullanıcılar** : ${message.guild.members.cache.filter(o => o.presence.status === 'online').size}`)
-      
-  .addField("**Toplam Kullanıcı**",message.guild.memberCount )
+   .addField("**Toplam Kullanıcı**",message.guild.memberCount )
+    .addField("**Çevrimiçi Kullanıcılar**",message.guild.members.cache.filter(o => o.presence.status === 'online').size)
+  .addField("**Boşta Kullanıcılar**",message.guild.members.cache.filter(o => o.presence.status === 'idle').size)
+.addField("**Rahatsız Etmeyindeki Kullanıcılar**",message.guild.members.cache.filter(o => o.presence.status === 'dnd').size)
+.addField("**Çevrimdışı Kullanıcılar**",message.guild.members.cache.filter(o => o.presence.status === 'offline').size)
+
     .setTimestamp()
   return message.channel.send(üye);
 };
