ab577b657179c120cbda9b60f6cc300f252307c6
Glitch (discordjsv12basitaltyapi) <none>
2020-09-01 10:25:36 +0000

    🌖🌔 Checkpoint ./komutlar/yardım.js:17207049/318 ./komutlar/botbilgi.js:17207049/391 ./bot.js:17905697/980

--- komutlar/yardım.js
+++ komutlar/yardım.js
@@ -7,9 +7,9 @@ exports.run = async (client, message, args) => {
 const yardım = new Discord.MessageEmbed()
 .setColor("RANDOM")
 .setAuthor("Destiny Bot v12 Sürümüyle Yeniden Sizlerle.")
-.setTitle("Destiny'in yardım menüsüne hoşgeldin!")
+.setTitle("<a:hypesquad:742698007262396426> Destiny'in yardım menüsüne hoşgeldin! <a:hypesquad:742698007262396426>")
  .setTimestamp()
-.setDescription("<a:yan:750065080741134407> **!eğlence** = Eğlence komutlarını açarsınız. \n <a:yan:750065080741134407> **!moderasyon** = komutlarını açarsınız. \n <a:yan:750065080741134407> **!kullanıcı** = Kullanıcı komutlarını açarsınız.")
+.setDescription("<a:yan:750065080741134407> **!eğlence** = Eğlence komutlarını açarsınız. <a:developeremoji:749525084586115153>  \n <a:yan:750065080741134407> **!moderasyon** = Moderasyon komutlarını açarsınız. <a:developeremoji:749525084586115153>  \n <a:yan:750065080741134407> **!kullanıcı** = Kullanıcı komutlarını açarsınız. <a:developeremoji:749525084586115153> ")
 .setImage("https://cdn.discordapp.com/attachments/740182427426881538/750289660693184562/Night_Sky_Mountain_Decorative_Border_Youtube_Thumbnail.png")
 message.channel.send(yardım)
 }
