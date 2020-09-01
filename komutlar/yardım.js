b2d264fa6a936231483782d49db1638c75102827
Glitch (discordjsv12basitaltyapi) <none>
2020-09-01 10:13:41 +0000

    🍽🕶 Checkpoint ./komutlar/logo.js:17207049/1439 ./komutlar/dinamik.js:17207049/582 ./komutlar/altın.js:17207049/546 ./komutlar/gaming.js:17207049/679 ./komutlar/yardım.js:17207049/83

--- komutlar/yardım.js
+++ komutlar/yardım.js
@@ -9,7 +9,7 @@ const yardım = new Discord.MessageEmbed()
 .setAuthor("Destiny Bot v12 Sürümüyle Yeniden Sizlerle.")
 .setTitle("Destiny'in yardım menüsüne hoşgeldin!")
  .setTimestamp()
-.setDescription("**!eğlence** = Eğlence komutlarını açarsınız. \n **!moderasyon** = komutlarını açarsınız. \n **!kullanıcı** = Kullanıcı komutlarını açarsınız.")
+.setDescription("<a:yan:750065080741134407> **!eğlence** = Eğlence komutlarını açarsınız. \n <a:yan:750065080741134407> **!moderasyon** = komutlarını açarsınız. \n <a:yan:750065080741134407> **!kullanıcı** = Kullanıcı komutlarını açarsınız.")
 .setImage("https://cdn.discordapp.com/attachments/740182427426881538/750289660693184562/Night_Sky_Mountain_Decorative_Border_Youtube_Thumbnail.png")
 message.channel.send(yardım)
 }
