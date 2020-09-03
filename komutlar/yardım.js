17f01df7e6305fe7c1d08653c4ddb775123b94c2
Glitch (discordjsv12basitaltyapi) <none>
2020-09-03 14:48:45 +0000

    🌨👮 Checkpoint ./komutlar/yardım.js:17207049/324

--- komutlar/yardım.js
+++ komutlar/yardım.js
@@ -5,11 +5,11 @@ const db = require("quick.db");
 exports.run = async (client, message, args) => {
 
 const yardım = new Discord.MessageEmbed()
-.setColor("RANDOM")
+.setColor("BLUE")
 .setAuthor("Destiny Bot v12 Sürümüyle Yeniden Sizlerle.")
 .setTitle("<a:hypesquad:742698007262396426> Destiny'in yardım menüsüne hoşgeldin! <a:hypesquad:742698007262396426>")
  .setTimestamp()
-.setDescription("<a:yan:750065080741134407> **!eğlence** = __Eğlence komutlarını görüntülersiniz.__ \n <a:yan:750065080741134407> **!moderasyon** = __Moderasyon komutlarını görüntülersiniz.__ \n <a:yan:750065080741134407> **!kullanıcı** = __Kullanıcı komutlarını görüntülersiniz.__")
+.setDescription("<a:destinyhype:751090021859196968> **!eğlence** = __Eğlence komutlarını görüntülersiniz.__ \n <a:ayarlardestiny:751089592245158030> **!moderasyon** = __Moderasyon komutlarını görüntülersiniz.__ \n <a:partnerdestiny:751089594694631465> **!kullanıcı** = __Kullanıcı komutlarını görüntülersiniz.__")
 .setImage("https://cdn.discordapp.com/attachments/735861222791971016/751077508207607847/Destiny.gif")
 message.channel.send(yardım)
 }
