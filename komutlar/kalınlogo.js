6858b895cfdb6ed4355832e26ed3593c8c5068eb
Glitch (discordjsv12basitaltyapi) <none>
2020-09-03 16:23:53 +0000

    👸🏤 Checkpoint ./komutlar/elmaslogo.js:17207049/2656 ./komutlar/yardım.js:17207049/182 ./komutlar/logo.js:17207049/125 ./komutlar/sarılogo.js:17207049/902 ./komutlar/neonmavi.js:17207049/1023 ./komutlar/kalınlogo.js:17207049/988

--- komutlar/kalınlogo.js
+++ komutlar/kalınlogo.js
@@ -4,7 +4,7 @@ exports.run = async (client, message, args) => {
   const yazi = args.slice(0).join('+'); 
 
   if(!yazi) return message.channel.send(`**Lütfen yazı yazınız.** <a:Yldz:742698148329291826>`)
-  const linqo = `https://habbofont.net/font/palooza_blue/${yazi}.gif`
+  const linqo = `https://habbofont.net/font/shalimar_big/${yazi}.gif`
   .replace(' ', '+')
 
   
@@ -12,7 +12,7 @@ exports.run = async (client, message, args) => {
   .setTitle("Logo")
   .setColor("RANDOM")
   .setImage(linqo)
-  .setFooter('Elmas Logo Oluşturuldu')
+  .setFooter('Kalın Logo Oluşturuldu')
   message.channel.send(embed)
 }
 exports.conf = {
@@ -23,7 +23,7 @@ exports.conf = {
 }
 
 exports.help = {
-    name: 'elmas',
-    description: 'Yazdığınız yazıyı dinamik çevirir.',
-    usage: 'elmas <yazı>'
+    name: 'kalın',
+    description: 'Yazdığınız yazıyı bannera çevirir.',
+    usage: 'kalın <yazı>'
 }
