eca0bc0024cfd911b834221c21c120f22b6bcdbc
Glitch (discordjsv12basitaltyapi) <none>
2020-09-03 14:14:21 +0000

    🛬🛥 Checkpoint ./komutlar/yardım.js:17207049/628 ./ayarlar.json:17207049/26

--- komutlar/yardım.js
+++ komutlar/yardım.js
@@ -9,7 +9,7 @@ const yardım = new Discord.MessageEmbed()
 .setAuthor("Destiny Bot v12 Sürümüyle Yeniden Sizlerle.")
 .setTitle("<a:hypesquad:742698007262396426> Destiny'in yardım menüsüne hoşgeldin! <a:hypesquad:742698007262396426>")
  .setTimestamp()
-.setDescription("<a:yan:750065080741134407> **!eğlence** = __**Eğlence komutlarını açarsınız.**__ <a:developeremoji:749525084586115153> \n <a:yan:750065080741134407> **!moderasyon** = __**Moderasyon komutlarını açarsınız.**__ <a:developeremoji:749525084586115153> \n <a:yan:750065080741134407> **!kullanıcı** = __**Kullanıcı komutlarını açarsınız.**__ <a:developeremoji:749525084586115153> \n <a:yan:750065080741134407>  __Müzik Komutları__  \n <a:yan:750065080741134407> **!müzik**")
+.setDescription("<a:yan:750065080741134407> __Eğlence Komutları__ \n  <a:yan:750065080741134407>  **!eğlence**                                         <a:yan:750065080741134407> __Moderasyon Komutları__ \n  <a:yan:750065080741134407> **!moderasyon** \n <a:yan:750065080741134407> __Kullanıcı Komutları__ \n <a:yan:750065080741134407> **!kullanıcı**")
 .setImage("https://media.giphy.com/media/W2Pi7elRIJ3HZmSnhm/giphy.gif")
 message.channel.send(yardım)
 }
