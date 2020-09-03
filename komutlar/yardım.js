68b91876456e96a2f8a8d124abd35255da185308
Glitch (discordjsv12basitaltyapi) <none>
2020-09-03 15:26:38 +0000

    🚄👞 Checkpoint ./komutlar/yardım.js:17207049/6 ./komutlar/moderasyon.js:17207049/26 ./komutlar/kullanıcı.js:17207049/18 ./komutlar/eğlence.js:17207049/16

--- komutlar/yardım.js
+++ komutlar/yardım.js
@@ -9,7 +9,7 @@ const yardım = new Discord.MessageEmbed()
 .setAuthor("Destiny v12 sürümüyle yeniden sizlerle.")
 .setTitle("<a:Yldz:742698148329291826> Destiny'in yardım menüsüne hoşgeldin! <a:Yldz:742698148329291826>")
  .setTimestamp()
-.setDescription("<a:destinyhype:751090021859196968> **!eğlence** = __Eğlence komutlarını görüntülersiniz.__ \n <a:ayarlardestiny:751089592245158030> **!moderasyon** = __Moderasyon komutlarını görüntülersiniz.__ \n <a:partnerdestiny:751089594694631465> **!kullanıcı** = __Kullanıcı komutlarını görüntülersiniz.__")
+.setDescription("<a:destinyhype:751090021859196968> **-eğlence** = __Eğlence komutlarını görüntülersiniz.__ \n <a:ayarlardestiny:751089592245158030> **-moderasyon** = __Moderasyon komutlarını görüntülersiniz.__ \n <a:partnerdestiny:751089594694631465> **-kullanıcı** = __Kullanıcı komutlarını görüntülersiniz.__")
 .setImage("https://media.giphy.com/media/PjCOJ0BjiGW5fJD3Wv/giphy.gif")
 message.channel.send(yardım)
 }
