5f14e8ef16bc70974703797c49d62507f96798ca
Glitch (discordjsv12basitaltyapi) <none>
2020-11-07 15:21:54 +0000

    🏄🎰 Checkpoint ./komutlar/yardım.js:2849939/3067 ./LICENSE.md:2849939/1149

--- komutlar/yardım.js
+++ komutlar/yardım.js
@@ -1,29 +1,61 @@
 const Discord = require("discord.js");
-const db = require("quick.db");
+exports.run = (client, message) => {
+  const motionEmbed = new Discord.MessageEmbed()
+
+    .setAuthor(`${client.user.username} `, client.user.avatarURL)
+    .setColor("YELLOW")
+    .setThumbnail(
+      "https://cdn.discordapp.com/attachments/750999058864406608/755645272335450182/a_dd75e2cbe5ebfc02273a11d055c77431.gif"
+    )
+    .setDescription(
+      `Toplamda Botta **${client.commands.size}** Adet Komut Bulunuyor!` +
+        "\n\n `p?botbilgi` **Yazarak Botun İstatistiklerine Ulaşa Bilirsiniz.** \n `p?davet` **Yazarak Botun Davet Linkini Alırsınız.**"
+    )
+    .addField(
+      `➤ Komutlar`,
+      `
+ 
+:white_small_square: | **p?eğlence:** Eğlence Menüsüne Bakarsınız.
+:white_small_square: | **p?moderasyon:** Botun Moderasyon Komutlarına Bakarsınız
+:white_small_square: | **p?moderasyon2:** Botun 2. Moderasyon Komutlarına Bakarsınız.
+:white_small_square: | **p?logomenü:** Botun Logo YAPMA Komutlarına Bakarsınız. 
+
+`)
 
 
-exports.run = async (client, message, args) => {
+    .addField(
+      "➤ Yardım & Hata & Bug  \n",
+      ":white_small_square: | **p?hatabildir:** "
+    )
 
-const yardım = new Discord.MessageEmbed()
-.setColor("BLUE")
-.setAuthor("Destiny v12 sürümüyle yeniden sizlerle.")
-.setTitle("<a:Yldz:742698148329291826> Destiny'in yardım menüsüne hoşgeldin! <a:Yldz:742698148329291826>")
- .setTimestamp()
-.setDescription("<a:destinyhype:751090021859196968> **-eğlence** = __Eğlence komutlarını görüntülersiniz.__ \n <a:ayarlardestiny:751089592245158030> **-moderasyon** = __Moderasyon komutlarını görüntülersiniz.__ \n <a:partnerdestiny:751089594694631465> **-kullanıcı** = __Kullanıcı komutlarını görüntülersiniz.__ \n <a:Yldz:742698148329291826> **-logo** = __Logo komutlarını görüntülersiniz.__")
-.setImage("https://cdn.discordapp.com/attachments/744536225230094360/751100899836821534/Night_Sky_Mountain_Decorative_Border_Youtube_Thumbnail.png")
-message.channel.send(yardım)
-}
+    .addField(
+      `   ¯\_(ツ)_/¯  Bilgilendirme`,
+      `
+:white_small_square: | **Phoneix Bot Her Gün Güncelleniyor!**.
+:white_small_square: | **Botu Ekleyerek Bize Destek Çıkmış Olursunuz.** `
+    )
+  
+  .addField(
+      `**➤ Bağlantılar  **`,
+      `>  » [Destek Sunucusu](https://discord.gg/eCJ27tG) \n >  » [Web Site](https://phoneixwebpanel.glitch.me/) \n > » [Botun Davet Linki](https://discord.com/oauth2/authorize?client_id=754272207907323944&scope=bot&permissions=2080767167)`
+    )
+    .setFooter(
+      `
+${message.author.username} Tarafından İstendi.`,
+      message.author.avatarURL
+    );
+  return message.channel.send(motionEmbed);
+};
 
 exports.conf = {
-  enabled: true, 
-  guildOnly: false, 
-   aliases: ["help","y"],
-  permLevel: `Yetki gerekmiyor.` 
+  enabled: true,
+  guildOnly: true,
+  aliases: ["h"],
+  permLevel: 0
 };
 
 exports.help = {
-  name: 'yardım',
-  category: 'kullanıcı',
-  description: 'Yardım Menüsü.',
-   usage:'!yardım'
-}
+  name: "yardım",
+  description: ".",
+  usage: "yardım"
+};
