eae3b8fca28311ce44c740cc3daafd51e9caebdb
Glitch (discordjsv12basitaltyapi) <none>
2020-12-24 14:16:56 +0000

    🌌🍃 Checkpoint ./komutlar/yardım.js:19828984/346 ./ayarlar.json:19828984/97 ./package.json:19828984/1027 ./LICENSE.md:19828984/1122 ./bot.js:19828984/24

--- bot.js
+++ bot.js
@@ -36,7 +36,7 @@ client.on("message", message => {
 client.on("ready", () => {
   console.log(`Bütün komutlar başarıyla yüklendi!`);
   client.user.setStatus("dnd");
-  client.user.setActivity('-yardım | -davet');
+  client.user.setActivity('m!yardım | m!davet');
 })
 
 
--- komutlar/yardım.js
+++ komutlar/yardım.js
@@ -3,41 +3,41 @@ exports.run = (client, message) => {
   const motionEmbed = new Discord.MessageEmbed()
 
     .setAuthor(`${client.user.username} `, client.user.avatarURL)
-    .setColor("YELLOW")
+    .setColor("RED")
     .setThumbnail(
       "https://cdn.discordapp.com/attachments/750999058864406608/755645272335450182/a_dd75e2cbe5ebfc02273a11d055c77431.gif"
     )
     .setDescription(
       `Toplamda Botta **${client.commands.size}** Adet Komut Bulunuyor!` +
-        "\n\n `p?botbilgi` **Yazarak Botun İstatistiklerine Ulaşa Bilirsiniz.** \n `p?davet` **Yazarak Botun Davet Linkini Alırsınız.**"
+        "\n\n `m!botbilgi` **Yazarak Botun İstatistiklerine Ulaşa Bilirsiniz.** \n `m!davet` **Yazarak Botun Davet Linkini Alırsınız.**"
     )
     .addField(
       `➤ Komutlar`,
       `
  
-:white_small_square: | **p?eğlence:** Eğlence Menüsüne Bakarsınız.
-:white_small_square: | **p?moderasyon:** Botun Moderasyon Komutlarına Bakarsınız
-:white_small_square: | **p?moderasyon2:** Botun 2. Moderasyon Komutlarına Bakarsınız.
-:white_small_square: | **p?logomenü:** Botun Logo YAPMA Komutlarına Bakarsınız. 
+:white_small_square: | **m!eğlence:** Eğlence Menüsüne Bakarsınız.
+:white_small_square: | **m!moderasyon:** Botun Moderasyon Komutlarına Bakarsınız
+:white_small_square: | **m!moderasyon2:** Botun 2. Moderasyon Komutlarına Bakarsınız.
+:white_small_square: | **m!logomenü:** Botun Logo YAPMA Komutlarına Bakarsınız. 
 
 `)
 
 
     .addField(
       "➤ Yardım & Hata & Bug  \n",
-      ":white_small_square: | **p?hatabildir:** "
+      ":white_small_square: | **m!hatabildir:** "
     )
 
     .addField(
       `   ¯\_(ツ)_/¯  Bilgilendirme`,
       `
-:white_small_square: | **Phoneix Bot Her Gün Güncelleniyor!**.
+:white_small_square: | **mays Bot Her Gün Güncelleniyor!**.
 :white_small_square: | **Botu Ekleyerek Bize Destek Çıkmış Olursunuz.** `
     )
   
   .addField(
       `**➤ Bağlantılar  **`,
-      `>  » [Destek Sunucusu](https://discord.gg/eCJ27tG) \n >  » [Web Site](https://phoneixwebpanel.glitch.me/) \n > » [Botun Davet Linki](https://discord.com/oauth2/authorize?client_id=754272207907323944&scope=bot&permissions=2080767167)`
+      `>  » [Destek Sunucusu](https://discord.gg/74MZyfZ24d) \n >  » > » [Botun Davet Linki](https://discord.com/api/oauth2/authorize?client_id=791609110658547782&permissions=8&scope=bot)`
     )
     .setFooter(
       `
