64e0cbf1c28b72d39795c767858a2c38e4dfd084
Glitch (discordjsv12basitaltyapi) <none>
2020-12-24 21:33:16 +0000

    ⛴🏏 Checkpoint ./komutlar/eğlence.js:19828984/283 ./komutlar/botbilgi.js:19828984/494 ./komutlar/davet.js:19828984/305

--- komutlar/davet.js
+++ komutlar/davet.js
@@ -4,8 +4,8 @@ exports.run = (client, message, args) => {
 
   const davet = new Discord.MessageEmbed()
   .setColor("RANDOM")
-  .setTitle("Beni Sunucuna Ekle ❤️")
-  .setDescription("[__**Davet Et**__](https://discord.com/api/oauth2/authorize?client_id=722377982773035020&permissions=8&scope=bot) \n [__**Destek Sunucusu**__](https://discord.gg/crBgspj)")
+  .setTitle("Beni Sunucuna Eklersen çok sevinirim ❤️")
+  .setDescription("[__**Davet Et**__](https://discord.com/api/oauth2/authorize?client_id=791609110658547782&permissions=8&scope=bot) \n [__**Destek Sunucusu**__](https://discord.gg/74MZyfZ24d)")
   message.channel.send(davet)
 }
 
--- komutlar/eğlence.js
+++ komutlar/eğlence.js
@@ -6,10 +6,10 @@ exports.run = async (client, message, args) => {
 
 const eğlence = new Discord.MessageEmbed()
 .setColor("RANDOM")
-.setAuthor("Morsürümüyle yeniden sizlerle.")
-.setTitle(" Destiny Eğlence Komutları ")
+.setAuthor("Mays v12 sürümüyle sizlerle")
+.setTitle(" ➤Mays Eğlence komutları" )
  .setTimestamp()
-.setDescription(" **m!tersyazı** = Bir Yazıyı Bot Ters Yazar. < \n  **m!mcskin** = Yazdığınız ismin minecraft görünüşünü atar.  \n  **m!fbi** = Bot FBi Gif Atar.  **m!token** = Tokenimi Öğrenmek İstemezmisin?  \n  **m!düello** = Düello Atarsın.  \n  **m!wasted** = Polis tarafından yakalanırsın.  \n  **m!atatürk** = Dene ve gör... (1881-1938) ")
+.setDescription(" **m!tersyazı** = Bir Yazıyı Bot Ters Yazar. < \n  **m!mcskin** = Yazdığınız ismin minecraft görünüşünü atar.  \n  **m!fbi** = Bot FBi Gif Atar. \n **m!token** = Tokenimi Öğrenmek İstemezmisin?  \n  **m!düello** = Düello Atarsın.  \n  **m!wasted** = Polis tarafından yakalanırsın.  \n  **m!atatürk** = Dene ve gör... (1881-1938) ")
 .setImage("https://cdn.discordapp.com/attachments/744536225230094360/751100899836821534/Night_Sky_Mountain_Decorative_Border_Youtube_Thumbnail.png")
 message.channel.send(eğlence)
 }
