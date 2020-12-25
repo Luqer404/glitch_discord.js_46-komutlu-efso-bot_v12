9b26a3f175e11fe323885eb54a52ba627255ce81
Glitch (discordjsv12basitaltyapi) <none>
2020-12-25 09:21:16 +0000

    🌇👨‍❤️‍💋‍👨 Checkpoint ./komutlar/moderasyon.js:19828984/47 ./komutlar/logomenü.js:19828984/1472 ./komutlar/eğlence.js:19828984/21

--- komutlar/logomenü.js
+++ komutlar/logomenü.js
@@ -7,9 +7,9 @@ exports.run = async (client, message, args) => {
 const eğlence = new Discord.MessageEmbed()
 .setColor("RANDOM")
 .setAuthor(" Mays v12 sürümüyle sizlerle")
-.setTitle(" ➤Mays Moderasyon komutları" )
+.setTitle(" ➤Mays logo menüsü komutları" )
  .setTimestamp()
-.setDescription(" **m!ping** \n **m!forceban** \n  **m!ban** \n **m!unban** \n **m!id** \n **m!küfür** \n **m!botbilgi** \n **m!sa-as** \n **m!kick** \n **m!sunucubilgi** \n **m!sil** \n **m!profil** \n **m!token** \n **m!yetkilerim** \n **m!link** \n  **m!üyedurum**\n **m!afk** \n **m!davet** ")
+.setDescription("**m!alev** \n **m!altın** \n **m!anime** \n **m!arrow** \n **m!banner**  \n **m!basit** \n **m!dinamik** \n **m!elmas** \n **m!greenlogo** \n  ")
 .setImage("https://cdn.discordapp.com/attachments/790999702765961258/791798016583008297/wp2490739.png")
 message.channel.send(eğlence)
 }
@@ -22,8 +22,8 @@ exports.conf = {
 };
 
 exports.help = {
-  name: 'moderasyon',
-  category: 'moderasyon',
+  name: 'logomenü',
+  category: 'logomenü',
   description: 'Yardım Menüsü.',
-   usage:'moderasyon'
+   usage:'logomenü'
 }
