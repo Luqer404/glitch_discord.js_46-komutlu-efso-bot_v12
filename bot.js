59fae0364edc8c5d8febf05deca4289ed5d41bf7
Glitch (discordjsv12basitaltyapi) <none>
2021-01-02 19:50:26 +0000

    🚦📚 Checkpoint ./komutlar/otorol.js:19828984/16 ./bot.js:19828984/1296

--- bot.js
+++ bot.js
@@ -205,39 +205,3 @@ client.login(ayarlar.token)
 
 
 
-
- client.on("guildMemberAdd", async member => {
-        let sayac = JSON.parse(fs.readFileSync("./autorole.json", "utf8"));
-  let otorole =  JSON.parse(fs.readFileSync("./autorole.json", "utf8"));
-      let arole = otorole[member.guild.id].sayi
-  let giriscikis = JSON.parse(fs.readFileSync("./autorole.json", "utf8"));  
-  let embed = new Discord.RichEmbed()
-    .setTitle('Otorol Sistemi')
-    .setDescription(`:loudspeaker: :inbox_tray:  @${member.user.tag}'a Otorol Verildi `)
-.setColor("GREEN")
-    .setFooter("XiR", client.user.avatarURL);
-
-  if (!giriscikis[member.guild.id].kanal) {
-    return;
-  }
-
-  try {
-    let giriscikiskanalID = giriscikis[member.guild.id].kanal;
-    let giriscikiskanali = client.guilds.get(member.guild.id).channels.get(giriscikiskanalID);
-    giriscikiskanali.send(`:loudspeaker: :white_check_mark: Hoşgeldin **${member.user.tag}** Rolün Başarıyla Verildi.`);
-  } catch (e) { // eğer hata olursa bu hatayı öğrenmek için hatayı konsola gönderelim.
-    return console.log(e)
-  }
-
-});
-//Kullanıcı sunucudan ayrıldığında ayarlanan kanala mesaj gönderelim.
-client.on("guildMemberAdd", async (member) => {
-      let autorole =  JSON.parse(fs.readFileSync("./autorole.json", "utf8"));
-      let role = autorole[member.guild.id].sayi
-
-      member.addRole(role)
-
-
-
-
-});
