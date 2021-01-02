29ae68c9602b26e3c1a3cf066858a1d1d4d28847
Glitch (discordjsv12basitaltyapi) <none>
2021-01-02 15:08:52 +0000

    💪💁 Checkpoint ./bot.js:19828984/10

--- bot.js
+++ bot.js
@@ -36,7 +36,7 @@ client.on("message", message => {
 client.on("ready", () => {
   console.log(`Bütün komutlar başarıyla yüklendi!`);
   client.user.setStatus("dnd");
-  client.user.setActivity('m!yardım | m!davet | m!is');
+  client.user.setActivity('m!yardım | m!davet | m!botbilgi');
 })
 
 
