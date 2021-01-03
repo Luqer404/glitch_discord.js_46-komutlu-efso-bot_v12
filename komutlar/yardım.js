73b4dfffd157d783a938789f60dbca09a70e502a
Glitch (discordjsv12basitaltyapi) <none>
2021-01-03 10:22:29 +0000

    🍥💍 Checkpoint ./komutlar/yardım.js:19828984/165

--- komutlar/yardım.js
+++ komutlar/yardım.js
@@ -33,7 +33,7 @@ exports.run = (client, message) => {
   
   .addField(
       `**:boom:➤ Bağlantılar  **`,
-      `>  » [Destek Sunucusu](https://discord.gg/74MZyfZ24d) \n >  » [Botun Davet Linki](https://discord.com/api/oauth2/authorize?client_id=791609110658547782&permissions=8&scope=bot) \n 
+      `>  » [Destek Sunucusu](https://discord.gg/74MZyfZ24d) \n >  » [Botun Davet Linki](https://discord.com/api/oauth2/authorize?client_id=791609110658547782&permissions=8&scope=bot) \n > » [Oy Verebilirsiniz](https://top.gg/bot/791609110658547782/vote) `
     )
     .setFooter(
       `
